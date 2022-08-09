20680dae062c62bb977d50749ddc97bac5e5fccc
Yorí <yorigcsdev@gmail.com>
2022-08-09 16:43:16 -0300

    feat: create HandleRoutes -> decide wich route to render

---
+++ src/routes/HandleRoutes.jsx
@@ -0,0 +1,8 @@
+import { PrivateRoutes } from './PrivateRoutes'
+import { PublicRoutes } from './PublicRoutes'
+import { useAuth } from '../contexts/auth'
+
+export const HandleRoute = () => {
+    const { signed } = useAuth();
+    return signed ? <PrivateRoutes /> : <PublicRoutes />
+}

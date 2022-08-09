12765cc5e08a3985811baaf39f8c1dafb1c1357b
Yorí <yorigcsdev@gmail.com>
2022-08-09 14:37:28 -0300

    feat: create signUpRequest

--- src/services/apiRequests.js
+++ src/services/apiRequests.js
@@ -3,3 +3,7 @@ import { api } from "./api";
 export const signInRequest = async (data) => {
     return api.post('/signIn',data)
 }
+
+export const signUpRequest = async (data) => {
+    return api.post('/signUp',data)
+}

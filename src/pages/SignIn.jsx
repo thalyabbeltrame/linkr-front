4ea0db334748e9c9cc01a87324dce3d2c5cda69f
Yorí <yorigcsdev@gmail.com>
2022-08-09 14:17:36 -0300

    refactor: change to individual paste

--- src/pages/SignIn.jsx
+++
@@ -1,8 +0,0 @@
-import { ContainerForm } from "../shared/ContainerForm"
-import { SignInForm } from "./SignInForm"
-export const SignIn = () => (
-    <ContainerForm>
-        <SignInForm />
-    </ContainerForm>
-)
-

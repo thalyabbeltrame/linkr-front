5731f44892cc21cf639266f4c4463704ebe93649
Yorí <yorigcsdev@gmail.com>
2022-08-09 17:06:10 -0300

    refactor: ensure that user keeps signed if close browser/page

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,8 +1,20 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { api } from "../services/api";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const storagedUser = localStorage.getItem("LinkrAuthUser");
+        const storagedToken = localStorage.getItem("LinkrAuthToken");
+
+        if (storagedUser && storagedToken) {
+            //send token if user is authenticated!
+            api.defaults.headers["Authorization"] = `Bearer ${storagedToken}`
+            setUserData(JSON.parse(storagedUser));
+        }
+    }, [])
 
     return (
         <AuthContext.Provider

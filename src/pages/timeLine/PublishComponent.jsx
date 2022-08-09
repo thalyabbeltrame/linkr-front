993f86c93345fb48b70456c4057974b374ec7908
Ruda <ruda.rabello.silva@gmail.com>
2022-08-09 18:34:04 -0300

    refactor: publish component

--- src/pages/timeLine/PublishComponent.jsx
+++ src/pages/timeLine/PublishComponent.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Img from '../../assets/logoDriven.png';
 
 export const PublishComponent = () => {
+
   return (
-    <div>PublishComponent</div>
+    <>
+    <Content>
+      <ImgContainer>
+        <img src={Img}></img>
+      </ImgContainer>
+      <InputsContainer>
+      
+      </InputsContainer>
+    </Content>
+    </>
   )
 }
+
+const Content = styled.div`
+display: flex;
+flex-direction: row;
+background: #FFFFFF;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 16px;
+
+`;
+const ImgContainer = styled.div`
+display: flex;
+flex-direction: row;
+background: #FFFFFF;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 16px;
+width: 50px;
+height: 50px;
+background: url(image.png);
+border-radius: 26.5px;
+
+`;
+
+const InputsContainer = styled.div`
+display: flex;
+flex-direction: row;
+background: #FFFFFF;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 16px;
+`;
